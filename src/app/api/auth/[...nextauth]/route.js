a16e426a0cf0df511c9c3ef277937b5013bd6adb
allagui_fawzi@outlook.fr <allagui_fawzi@outlook.fr>
2023-08-18 21:49:04 +0100

    rename routes.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,9 +4,9 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+          clientId: process.env.GOOGLE_CLIENT_ID??"",
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET??"",
+        }),
       ]
     })
 
