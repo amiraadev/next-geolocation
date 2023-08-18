46be17a7d84bed48089f96158c88d9117f43fde0
allagui_fawzi@outlook.fr <allagui_fawzi@outlook.fr>
2023-08-18 18:56:57 +0100

    provider

---
+++ src/app/provider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SessionProvider } from "next-auth/react"
+
+function Provider({children}) {
+  return (
+    <div>
+      <SessionProvider>
+      {children}
+      </SessionProvider>
+    </div>
+  )
+}
+
+export default Provider
