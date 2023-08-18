6bd97118df6934b0c80eaf20e89b794267bf37a8
allagui_fawzi@outlook.fr <allagui_fawzi@outlook.fr>
2023-08-18 19:36:01 +0100

    login

---
+++ src/app/Login/index.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <Image src='/logo.png' alt="logo" width={100}  height={100}/>
+    </div>
+  )
+}
+
+export default Login
